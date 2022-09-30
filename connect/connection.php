e9c632ae9c7021c75683c4a8be5dcfba70c9114d
KevinHdezVaz <kevinhdezvaz@gmail.com>
2022-09-30 16:03:04 -0500

    a

---
+++ connect/connection.php
@@ -0,0 +1,23 @@
+<?php 
+
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "bdsaua";
+
+ /*
+$servidor= "localhost";
+$NombreDB = "bdsaua";
+$UsaurioDB = "usrsaua";
+$ContrasenaDB = "AmorMio15$&";
+*/
+
+$connect = new mysqli($dbServername, $dbUsername, $dbPassword, $dbName);
+
+/*
+// Check connection
+if ($mysqli -> connect_error) {
+  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+  exit();
+}*/
+?>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'e3429afe13f21c66763d015d4bc26f87d4815707',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e3429afe13f21c66763d015d4bc26f87d4815707',
+    ),
+    'phpmailer/phpmailer' => 
+    array (
+      'pretty_version' => 'v6.6.4',
+      'version' => '6.6.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a94fdebaea6bd17f51be0c2373ab80d3d681269b',
+    ),
+  ),
+);
